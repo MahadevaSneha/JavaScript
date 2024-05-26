15490c31bbed03847b5e2a4926bf7d39e2858035
Mahadeva Sneha <116667567+MahadevaSneha@users.noreply.github.com>
2024-05-26 15:14:41 +0530

    Create VariableConc.js

---
+++ VariableConc.js
@@ -0,0 +1,9 @@
+//global variable declaration
+let num1=50;
+function  myFunc(){
+	let num2=60;//local variable declaration
+    console.log(num1);
+    console.log(num2);
+}
+myFunc();//Function Call
+
