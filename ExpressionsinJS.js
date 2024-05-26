301173a0c5d63d51692f8b4dede13915ca292e37
Mahadeva Sneha <116667567+MahadevaSneha@users.noreply.github.com>
2024-05-26 15:24:59 +0530

    Create ExpressionsinJS.js

---
+++ ExpressionsinJS.js
@@ -0,0 +1,6 @@
+let n1,n2;//variable declaration
+n1=10;//Assigning the values to the variables
+n2=5;
+div=n1/n2 //performing divison operations using / operator ---Expression
+mul=n1*10 //performing multiplication operation using * operator --- Expression
+console.log(div+"\n"+mul)
