4224bdaa04c746f81ca8482b1001d1a4dcd88ed8
Mahadeva Sneha <116667567+MahadevaSneha@users.noreply.github.com>
2024-06-05 11:51:49 +0530

    Create NumericStrings.js

---
+++ NumericStrings.js
@@ -0,0 +1,12 @@
+let add="100"+"200";//100200 --->Concatenation
+let mul="10"*"10"//100 --->In Some Operations the  numeric strings are converted into numbers
+let div="100"/"10"//10
+let sub="100"-"50";//50
+let div1="100"/10;//10
+let mul1="100"*2;//200
+console.log(add);
+console.log(mul);
+console.log(div);
+console.log(sub);
+console.log(div1);
+console.log(mul1);
