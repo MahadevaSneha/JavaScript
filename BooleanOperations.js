ca9ce8c22375138be4352d71eb32d6a71fb4dc1b
Mahadeva Sneha <116667567+MahadevaSneha@users.noreply.github.com>
2024-06-05 20:55:49 +0530

    Create BooleanOperations.js

---
+++ BooleanOperations.js
@@ -0,0 +1,12 @@
+//Boolean operations
+const val1=true+true;
+const val2=false*true;
+const val3=false+10;
+const val4=true*true+10;
+const newValue=true;
+console.log(val1);//2
+console.log(val2);//0
+console.log(val3);//10
+console.log(val4);//11
+console.log(newValue);//true
+console.log(typeof newValue);//boolean
