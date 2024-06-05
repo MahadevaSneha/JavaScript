8d8f1ae8d03f4f28305e22c29a12eff81f694053
Mahadeva Sneha <116667567+MahadevaSneha@users.noreply.github.com>
2024-06-05 11:40:02 +0530

    Create AddandConcatenation.js

---
+++ AddandConcatenation.js
@@ -0,0 +1,13 @@
+//Additon of two numbers using + operator
+let a=10;
+let b=20;
+console.log(a+b)//30
+//Concatention of two strings using + operator
+let s1="Sneha";
+let s2="JayaSurya";
+console.log(s1+" "+s2);//Sneha JayaSurya
+
+//number string concatenation
+let v1="100";
+let v2="300";
+console.log(v1+v2);//100300
