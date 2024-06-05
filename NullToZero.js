c06afadbedaf5c28a5cefa815f0ed69904d55e9d
Mahadeva Sneha <116667567+MahadevaSneha@users.noreply.github.com>
2024-06-05 20:48:33 +0530

    Create NullToZero.js

---
+++ NullToZero.js
@@ -0,0 +1,9 @@
+//Null to 0
+const res=null+10;
+console.log(res)//10
+console.log(typeof res);//number
+
+//Assigning null to a global variable
+var value=null;
+console.log(value);//null
+console.log(typeof value);//object
