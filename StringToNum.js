8b53ce6f0bf87291cecf24942ee9e0e1faa1d9e1
Mahadeva Sneha <116667567+MahadevaSneha@users.noreply.github.com>
2024-06-05 21:05:41 +0530

    Create StringToNum.js

---
+++ StringToNum.js
@@ -0,0 +1,10 @@
+//Conversion of String number to number
+const str="22";
+const num=Number(str);
+console.log(num)//22
+
+//Conversion of string text to number -- returns NaN as output
+const str1="22Sneha";
+const str2="sneha";
+console.log(Number(str1));//Nan
+console.log(Number(str2));//Nan
