f13fd30cbe19d1cd84c778baebf1740b8f894fca
Mahadeva Sneha <116667567+MahadevaSneha@users.noreply.github.com>
2024-06-05 22:13:31 +0530

    Create NumberMethod.js

---
+++ NumberMethod.js
@@ -0,0 +1,6 @@
+let x=54;
+console.log(x.toString())//54
+console.log(x.toExponential(5));//5.40000e+1
+console.log(x.toPrecision(6));//54.0000
+console.log(Number.isInteger(x))//true
+console.log(x.toLocaleString("bn-BD"));
