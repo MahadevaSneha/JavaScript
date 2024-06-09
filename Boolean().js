962abbb71ab99ed12b5eb72848a7f14b50afb156
Mahadeva Sneha <116667567+MahadevaSneha@users.noreply.github.com>
2024-06-09 21:52:54 +0530

    Create Boolean().js

---
+++ Boolean().js
@@ -0,0 +1,8 @@
+//Boolean() function JS
+function myFunction(){
+  console.log(Boolean("10"));//true
+  console.log(Boolean(""));//false
+  console.log(Boolean(0));//false
+  console.log(Boolean(1<2));//true
+}
+myFunction();//function call
