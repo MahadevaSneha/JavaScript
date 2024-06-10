4fa33bac58c5d0eb114144d5583e1db1e29c4844
Mahadeva Sneha <116667567+MahadevaSneha@users.noreply.github.com>
2024-06-10 11:35:53 +0530

    Create nullldt.js

---
+++ nullldt.js
@@ -0,0 +1,8 @@
+ const var1=null;
+console.log("The datatype of var1 is:",typeof var1);//The datatype of var1 is: object
+if(var1){
+  console.log("Var1 is not null");
+}
+else{
+  console.log("Var2 is null");
+}//Here the null is falsy value so that if conidtion can't be extecuted 
