72e635e5b82e55a02912cf6149942b5111167add
Mahadeva Sneha <116667567+MahadevaSneha@users.noreply.github.com>
2024-06-10 16:38:15 +0530

    Create undefineinJs.js

---
+++ undefineinJs.js
@@ -0,0 +1,27 @@
+//Checking the variable having the undefined value or not
+let myvar;
+if(myvar==undefined){
+  console.log("myvar is undefined");
+}else{
+  console.log("myvar is defined");
+}
+//Another approach
+if(typeof myvar==="undefined"){
+  console.log("myvar is undefined");
+}else{
+  console.log("myvar is defined");
+}
+//another approach
+if(typeof myvar===undefined){
+  console.log("myvar is undefined");
+}
+else{
+  console.log("myvar is defined");
+}
+//another approach
+if(myvar == "undefined"){
+  console.log("myvar is undefined");
+}
+else{
+  console.log("myvar is defined");
+}
