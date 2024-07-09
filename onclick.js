83de7afef3a18bfd7382b15d818d62b755b90ba4
Mahadeva Sneha <116667567+MahadevaSneha@users.noreply.github.com>
2024-07-09 09:49:13 +0530

    Create onclick.js

---
+++ onclick.js
@@ -0,0 +1,11 @@
+<html>
+  <head>
+    <title>
+      JS
+    </title>
+  </head>
+  <body>
+    <img src="http://www.sololearn.com/images/tree.jpg" id="tree"><br><br>
+    <button onclick="tree.src='https://blob.sololearn.com/courses/robot-c.jpg'">Click me</button>
+  </body>
+</html>
